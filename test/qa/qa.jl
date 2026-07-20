f2dcff953a6082eb483764a8aa2d2536c52e4ed6
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-20 10:32:07 -0400

    Use default explicit import QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,7 +2,6 @@ using SciMLTesting, TupleLU, JET
 
 run_qa(
     TupleLU;
-    explicit_imports = true,
     aqua_broken = (:unbound_args,),  # (TupleMatrix{M,N})(x::NTuple{L,T}) trips test_unbound_args — https://github.com/SciML/TupleLU.jl/issues/8
     jet_broken = true,               # similar_type undefined in getproperty + degenerate TupleMatrix __lu ctors — https://github.com/SciML/TupleLU.jl/issues/8
     jet_kwargs = (; target_defined_modules = true),
